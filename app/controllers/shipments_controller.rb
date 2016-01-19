4ddf54e6d11f8bf09bada1348c41f59b8ec582d9
Lauren Granger <lr.granger21@gmail.com>
2016-01-19 14:49:55 -0800

    mocked out methods for getting in data and sending out to USPS and UPS api's

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,2 +1,15 @@
 class ShipmentsController < ApplicationController
+
+  def estimate
+    shipment = Shipment.new(shipment_params)
+    shipment.get_shipping_cost
+    #return both datas as JSON
+  end
+
+private
+
+  def shipment_params
+    params.require(:shipment).permit(:name, :country, :city, :state, :postal_code, :length, :width, :height, :weight, :cylinder)
+  end
+
 end
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,2 +1,6 @@
 class Shipment < ActiveRecord::Base
+  def get_shipping_cost
+    #call USPS api and get data
+    #call UPS api and get data
+  end
 end
