74edda274ccc9e6c6bf8bb2dbf8b70111976256e
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-19 14:56:09 -0800

    started writing the rates method

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,2 +1,15 @@
 class PackagesController < ApplicationController
+
+  def rates
+    package = ActiveShipping::Package.new(100, [10, 20, 30], :units => :metric)
+    origin = ActiveShipping::Location.new(country: 'US',
+                                       state: 'CA',
+                                       city: 'Beverly Hills',
+                                       zip: '90210')
+
+    destination = ActiveShipping::Location.new(country: 'CA',
+                                            province: 'ON',
+                                            city: 'Ottawa',
+                                            postal_code: 'K1P 1J1')
+  end
 end
