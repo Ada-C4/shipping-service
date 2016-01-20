77c11173bd86c4c9388ccf8fd2348976cf1301c3
Ricky Hougland <houglande@gmail.com>
2016-01-19 16:17:44 -0800

    add factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,22 @@ FactoryGirl.define do
     price 100
     carrier "UPS"
     service_name "ground"
+    shipment
   end
 
+  factory :location do
+    country "US"
+    city "Seattle"
+    state "WA"
+    zip 98105
+  end
+
+  factory :package do
+    dimensions "[4, 5, 6]"
+    weight 456
+    shipment
+  end
+
+  factory :shipment do
+  end
 end
