d706269fdf4355756b6295059190b3569aca117d
Ricky Hougland <houglande@gmail.com>
2016-01-20 13:45:02 -0800

    shipping initializer method

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,26 +1,15 @@
 class ShipmentsController < ApplicationController
 
-def shipment
-  # maybe move some of this stuff to models?
-  shipment = Shipment.new
-  origin = Location.new(params[:origin])
-  destination = Location.new(params[:destination])
-  packages = params[:packages].each do |package|
-    Package.new(package)
+  def shipment
+    shipment = Shipment.create(params)
+    quotes = []
+    ups = ActiveShipping::UPS.new("put login and keys here")
+    ups_quote = ups.find_rates(shipment.origin, shipment.destination, shipment.packages)
+    fedex = ActiveShipping::FedEx.new("put login and keys here")
+    fedex_quote = "get quote from fedex"
+    quotes << ups_quote
+    quotes << fedex_quote
+    # return json info
+    # render :json => quotes.as_json
   end
-  shipment.origin = origin
-  shipment.destinatiopn = destination 
-  shipment.packages = packages
-  shipment.save
-  quotes = []
-  ups = ActiveShipping::UPS.new("put login and keys here")
-  ups_quote = ups.find_rates(origin, destination, packages)
-  fedex = ActiveShipping::FedEx.new("put login and keys here")
-  fedex_quote = "get quote from fedex"
-  quotes << ups_quote
-  quotes << fedex_quote
-  # return json info
-  # render :json => quotes.as_json
-end
-
 end
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,12 @@
 class Shipment < ActiveRecord::Base
   has_many :packages
   has_many :estimates
+  attr_reader :origin, :destination, :packages
+
+  def initialize(hash)
+    @origin = Location.new(params[:origin])
+    @destination = Location.new(params[:destination])
+    @packages = []
+    params[:packages].each { |package| @packages << Package.new(package) }
+  end
 end
