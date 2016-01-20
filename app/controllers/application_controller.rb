af828b0b37d255c63ff3ccbb81e8951dca576f93
Claire <ClaireS29@gmail.com>
2016-01-20 14:46:21 -0800

    initial rates for ups and usps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #query ups for prices
+  def ups_get_rates(orgin, destination, packages)
+    ups = ActiveShipping::UPS.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY'])
+    response = ups.find_rates(origin, destination, packages)
+    ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+  end
+
+  def usps_get_rates(orgin, destination, packages)
+    usps = ActiveShipping::USPS.new(:login => ENV['USPS_LOGIN'])
+    response = usps.find_rates(origin, destination, packages)
+    usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+  end
 end
