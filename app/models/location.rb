d2f5a529b8f1ac616378db00f69034766534cc87
Ricky Hougland <houglande@gmail.com>
2016-01-20 14:49:31 -0800

    fix inputs for find or create by method in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,16 +1,16 @@
 class Location < ActiveRecord::Base
   validates_presence_of :country, :city, :state, :zip
 
-  def self.find_or_create_by(params)
+  def self.find_or_create_by(hash)
     # return a Location if it exists, or make a new one
-    location = Location.find_by(country: params[:origin][:country], city: params[:origin][:city], state: params[:origin][:state], zip: params[:origin][:zip])
+    location = Location.find_by(country: hash[:country], city: hash[:city], state: hash[:state], zip: hash[:zip])
 
     if location.nil?
       location = Location.new
-      location.country = params[:origin][:country]
-      location.city = params[:origin][:city]
-      location.state = params[:origin][:state]
-      location.zip = params[:origin][:zip]
+      location.country = hash[:country]
+      location.city = hash[:city]
+      location.state = hash[:state]
+      location.zip = hash[:zip]
       if location.save
         return location
       else
