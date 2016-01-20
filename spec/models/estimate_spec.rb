0a31607c4ab27f98e2263dafee8141eec0a3e1b5
Ricky Hougland <houglande@gmail.com>
2016-01-19 16:24:49 -0800

    validation rspecs package and estimate

--- spec/models/estimate_spec.rb
+++ spec/models/estimate_spec.rb
@@ -10,11 +10,13 @@ RSpec.describe Estimate, type: :model do
     end
 
     it "has a carrier" do
-
+      expect(estimate).to be_valid
+      expect(build(:estimate, carrier: nil)).to be_invalid
     end
 
     it "has a service_name" do
-
+      expect(estimate).to be_valid
+      expect(build(:estimate, service_name: nil)).to be_invalid
     end
   end
 end
