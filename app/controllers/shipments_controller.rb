c374d535d80b0c51dbd47de89666f8dd3eb02471
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-20 10:02:34 -0800

    wrote out code/psuedocode for shipment api point

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,2 +1,21 @@
 class ShipmentsController < ApplicationController
+
+def shipment
+  shipment = Shipment.new
+  origin = Location.new(params[:origin])
+  destination = Location.new(params[:destination])
+  packages = params[:packages].each do |package|
+    Package.new(package)
+  end
+  quotes = []
+  ups = ActiveShipping::UPS.new()
+  ups_quote = ups.find_rates(origin, destination, packages)
+  fedex = ActiveShipping::FedEx.new()
+  fedex_quote = "get quote from fedex"
+  quotes << ups_quote
+  quotes << fedex_quote
+  # return json info
+  # render :json => quotes.as_json
+end
+
 end
