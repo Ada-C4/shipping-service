9be574ef3c2004fefb0d0378b42df2e1464118bc
Desiree <desireepoland@gmail.com>
2016-01-20 14:38:48 -0800

    add location factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,17 @@
 FactoryGirl.define do
-  factory :shipping_params do
-    username "foo"
-    email { "#{username}@example.com" }
-    password "foobar"
-    password_confirmation "foobar"
+
+  factory :origin do
+    country "US"
+    state "CA"
+    city "San Francisco"
+    postal_code "94132"
   end
 
+  factory :destination do
+    country "US"
+    state "WA"
+    city "Seattle"
+    postal_code "98161"
+  end
 
 end
