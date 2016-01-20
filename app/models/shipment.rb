995fbad1957101e62d9c0fb13d6c9eced1456bfb
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-20 14:11:18 -0800

    commented the lines we need to make estimates in the mdoel

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -14,8 +14,10 @@ class Shipment < ActiveRecord::Base
     quotes = []
     ups = ActiveShipping::UPS.new("put login and keys here")
     ups_quote = ups.find_rates(self.origin, self.destination, self.packages)
+    # make an estimate from each quote, using REGEX to pull UPS out of the string
     fedex = ActiveShipping::FedEx.new("put login and keys here")
     fedex_quote = "get quote from fedex"
+    # make estimate from eachquote, use regex to pull FEDEX out of string"
     quotes << ups_quote
     quotes << fedex_quote
     return quotes
