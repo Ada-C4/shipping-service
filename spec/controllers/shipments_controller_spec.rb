add216447d2911f64ff330368c63a21e446fe1ce
Lauren Granger <lr.granger21@gmail.com>
2016-01-20 14:01:22 -0800

    some let things for specs

--- spec/controllers/shipments_controller_spec.rb
+++ spec/controllers/shipments_controller_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ShipmentsController, type: :controller do
   describe "GET 'estimate'" do
+    let(:params) do
+      {origin: {country: 'US',
+                 state: 'CA',
+                 city: 'Beverly Hills',
+                 zip: '90210'},
+
+      destination: {country: 'CA',
+              province: 'ON',
+              city: 'Ottawa',
+              postal_code: 'K1P 1J1'}
+    end
+
     it "is successful" do
       get :estimate
       expect(response.response_code).to eq 200
