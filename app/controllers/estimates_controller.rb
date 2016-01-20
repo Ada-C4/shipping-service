7ca8a0b619b42785aa57e1541945689a8c1e4d11
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-20 08:59:30 -0800

    Stubbing methods for generating a quote and getting shipping params from client

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -2,9 +2,15 @@ class EstimatesController < ApplicationController
   include Estimator
 
   def make_quote
-    
+    QuoteCalulator.query(shipping_params)
   end
 
+
+
+private
+
+  def shipping_params
+  end
 
 
 end
