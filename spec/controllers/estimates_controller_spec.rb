c82049042161465058f44bc5a5a8f61ff5e2620c
Desiree <desireepoland@gmail.com>
2016-01-20 15:20:37 -0800

    Create specs for Estimates#quote

--- spec/controllers/estimates_controller_spec.rb
+++ spec/controllers/estimates_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe EstimatesController, type: :controller do
+  describe "GET 'quote'" do
+    it "is successful" do
+      get :quote
+      expect(response.response_code).to eq 200
+    end
 
+    it "returns json" do
+      get :quote
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+  end
 end
