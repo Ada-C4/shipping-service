22dc695862257d34685d61e720e38f9e4b3f09de
Lauren Granger <lr.granger21@gmail.com>
2016-01-19 16:12:58 -0800

    mocked out methods for getting in data and sending out to USPS and UPS api's

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,6 +1,23 @@
 class Shipment < ActiveRecord::Base
-  def get_shipping_cost
+
+  def package
+  end
+
+  def origin
+  end
+
+  def destination
+  end
+
+  def USPS_shipping_cost(package, origin, destination)
     #call USPS api and get data
+  end
+
+  def UPS_shipping_cost(package, origin, destination)
     #call UPS api and get data
+  end
+
+  def shipping_cost(USPS_shipping_cost, UPS_shipping_cost)
+    #combine data and format to JSON
   end
 end
