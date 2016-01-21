43f54c21930dd4d28d6c9703ea7a6bc787bca675
Claire <ClaireS29@gmail.com>
2016-01-21 11:46:26 -0800

    made a shipping estimates view and changed the default port of app to 3001

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   #query ups for prices
-  def prepare
-    
-    if shipper == "ups"
-      ups_get_rates
-    elsif shipper == "usps"
-      usps_get_rates
-    end
+  def get_rates(destination)
+    origin = ActiveShipping::Location.new(country: 'US', state: 'CA', city: 'Beverly Hills', zip: '90210')
+    packages = [ActiveShipping::Package.new(100, [93,10], cylinder: true)]
+    ups_get_rates(origin, destination, packages)
+    #TODO: separate out carriers
+    # if shipper == "ups"
+    #   ups_get_rates
+    # elsif shipper == "usps"
+    #   usps_get_rates
+    # end
   end
 
-  def ups_get_rates(orgin, destination, packages)
+  def prepare_origin
+  end
+
+  def ups_get_rates(origin, destination, packages)
+    destination = ActiveShipping::Location.new(country:, state:, city:, zip:)
     ups = ActiveShipping::UPS.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY'])
     response = ups.find_rates(origin, destination, packages)
     ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
