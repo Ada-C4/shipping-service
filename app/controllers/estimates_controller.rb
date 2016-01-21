4b3818c71dcf72c508147e56aaec848232358f4b
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-20 21:46:09 -0800

    Using written params instead of factory for tests

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -15,7 +15,7 @@ class EstimatesController < ApplicationController
 private
 
   def shipping_params
-    params.require(:shipping).permit(:destination[:country, :state, :city, :postal_code], :package[:origin[:country, :state, :city, :postal_code], :package_item[:weight, :length, :width, :height]])
+    params.require(:shipping).permit(:destination[:country, :state, :city, :postal_code], :package[:origin[:country, :state, :city, :postal_code], :package_item[:weight, :dimensions]])
   end
 
 
--- spec/controllers/estimates_controller_spec.rb
+++ spec/controllers/estimates_controller_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe EstimatesController, type: :controller do
   describe "GET 'quote'" do
+
+    let(:shipping_params) {
+      { destination:
+        { country: "US", state: "WA", city: "Seattle", postal_code: "98122"},
+      { package:
+        {:origin
+          { country: "US", state: "MA", city: "Hinsdale", postal_code: "01235"}
+          { weight: 12, dimensions: [15, 10, 4.5] }
+        }
+      }}}
+
+
     it "is successful" do
       get :quote
       expect(response.response_code).to eq 200
