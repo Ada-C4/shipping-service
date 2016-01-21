8175e48ba001fd84ce43c6692b780a99df2f896f
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-21 15:38:54 -0800

    create first draft estimate method for UPS

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -1,2 +1,36 @@
+require 'active_shipping'
+
 class EstimatesController < ApplicationController
+  # create constants for origin object, package grams_or_ounces and package dimensions, and country
+  # also create carrier constants which is a ups carrier and a fedex carrier?
+
+  UPS = ActiveShipping::UPS.new(login: 'UPS_LOGIN', password: 'UPS_PASSWORD', key: 'UPS_KEY')
+  FEDEX = ActiveShipping::FedEx.new(login: 'FEDEX_LOGIN', password: 'FEDEX_PASSWORD', key: 'FEDEX_KEY', account: 'FEDEX_ACCOUNT', test: true)
+
+  
+  # assume all packages are being sent within the US
+  COUNTRY = "US"
+  # assume all packages are originating from Ada's betsy distribution center
+  ORIGIN = ActiveShipping::Location.new(country: COUNTRY, state: 'WA', city: 'Seattle', zip: '98101')
+  # assume all packages have the same ounces and dimensions, for now
+  OUNCES = 120
+  DIMENSIONS = [15, 10, 4.5]
+
+  def estimate
+    # assume carrier is ups for now
+    # only one package for all products, for now
+      package = [ActiveShipping::Package.new(OUNCES, DIMENSIONS, units: :imperial, value: params[:value])]
+    # destination address info comes from query params provided from betsy app's API call
+      destination = ActiveShipping::Location.new(country: COUNTRY, state: params[:destination][:state], city: params[:destination][:city], zip: params[:destination][:zip])
+
+
+      rates_response = UPS.find_rates(ORIGIN, destination, package)
+      delivery_dates_response = UPS.get_delivery_date_estimates(ORIGIN, destination, package)
+
+      # need to filter above responses down to the data we want (cost, date, service code)
+      # also need to remove any results with nil for service code
+      response = [rates_response, delivery_dates_response]
+
+    render :json => response.as_json, :status => :ok
+  end
 end
--- spec/controllers/estimates_controller_spec.rb
+++ spec/controllers/estimates_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe EstimatesController, type: :controller do
-
+  describe 'estimate' do
+    context 'when carrier is ups' do
+      it 'returns '
+    end
+  end
 end
