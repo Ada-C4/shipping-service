adc252aac8a1c44180b53d15c10f1b81556d88bd
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-21 14:21:44 -0800

    error handled if nothing is submitted in request

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,21 +1,24 @@
 class ShipmentsController < ApplicationController
   def shipment
+    if params.include?("destination")
+      origin = ActiveShipping::Location.new(country: 'US', state: 'CA', city: 'Beverly Hills', zip: '90210')
 
-    origin = ActiveShipping::Location.new(country: 'US', state: 'CA', city: 'Beverly Hills', zip: '90210')
+      destination = ActiveShipping::Location.new(country: params[:destination][:country], state: params[:destination][:state], city: params[:destination][:city], zip: params[:destination][:zip])
+      packages = []
 
-    destination = ActiveShipping::Location.new(country: params[:destination][:country], state: params[:destination][:state], city: params[:destination][:city], zip: params[:destination][:zip])
-    packages = []
+      params[:packages].each do |package|
+        weight = package[:weight].to_i
+        dimensions = package[:dimensions].to_a.map! { |num| num.to_i }
 
-    params[:packages].each do |package|
-      weight = package[:weight].to_i
-      dimensions = package[:dimensions].to_a.map! { |num| num.to_i }
+        pack = ActiveShipping::Package.new(weight, dimensions)
+        packages << pack
+      end
 
-      pack = ActiveShipping::Package.new(weight, dimensions)
-      packages << pack
+      @quotes = get_quotes(origin, destination, packages)
+      render :json => @quotes.as_json
+    else 
+      render:json => ["You didn't submit the correct information."].as_json
     end
-
-    @quotes = get_quotes(origin, destination, packages)
-    render :json => @quotes.as_json
   end
 
   private
