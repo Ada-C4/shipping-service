6cf45bc564f4d1d880ead13b8c288aea7bd71e90
Daphne <dg1181@nyu.edu>
2016-01-20 19:53:09 -0800

    Added USPS example to controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,7 +1,8 @@
 class ServicesController < ApplicationController
   ## EXAMPLE
-    # params = { packages: [{ dimensions: [20, 10, 5], weight: 50 }, { dimensions: [15, 5, 10], weight: 25 }], origin: { state: "WA", city: "Seattle", zip: "98101" }, destination: { state: "IL", city: "Vernon Hills", zip: "60061" } }.to_json
+    # params = { packages: [{ dimensions: [25, 10, 15], weight: 500 }, { dimensions: [18, 30, 10], weight: 5000 }], origin: { state: "WA", city: "Seattle", zip: "98101" }, destination: { state: "IL", city: "Vernon Hills", zip: "60061" } }.to_json
     # r = HTTParty.post("http://localhost:3000/ups", headers: { 'Content-Type' => 'application/json' }, body: params)
+    # r = HTTParty.post("http://localhost:3000/usps", headers: { 'Content-Type' => 'application/json' }, body: params)
 
   def ship
     packages = set_packages
