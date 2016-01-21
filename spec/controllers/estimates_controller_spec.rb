14045deda3ea57cbe4b1eafbca4d92f10dfde008
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-20 22:17:19 -0800

    Got params hash to at least run in estimates controller

--- spec/controllers/estimates_controller_spec.rb
+++ spec/controllers/estimates_controller_spec.rb
@@ -3,15 +3,32 @@ require 'rails_helper'
 RSpec.describe EstimatesController, type: :controller do
   describe "GET 'quote'" do
 
-    let(:shipping_params) {
-      { destination:
-        { country: "US", state: "WA", city: "Seattle", postal_code: "98122"},
-      { package:
-        {:origin
-          { country: "US", state: "MA", city: "Hinsdale", postal_code: "01235"}
-          { weight: 12, dimensions: [15, 10, 4.5] }
+    let(:shipping_params) do
+      { destination =>
+        { :country => "US",
+          :state => "WA",
+          :city => "Seattle",
+          :postal_code => "98122"},
+        packages =>
+        {1 =>
+          { :origin =>
+            { :country => "US",
+              :state => "MA",
+              :city => "Hinsdale",
+              :postal_code => "01235"},
+            :weight => 12,
+            :dimensions => [15, 10, 4.5] },
+         2 =>
+          { :origin =>
+            { :country => "US",
+              :state => "CT",
+              :city => "New London",
+              :postal_code => "06320"},
+            :weight => 1,
+            :dimensions => [9, 10, 4.5] }
         }
-      }}}
+      }
+    end
 
 
     it "is successful" do
