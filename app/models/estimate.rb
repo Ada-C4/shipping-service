c6080af5e531efeda796d8615ad2a37204b7aa14
Ricky Hougland <houglande@gmail.com>
2016-01-21 13:36:00 -0800

    delete useless models

--- app/models/estimate.rb
+++
@@ -1,5 +0,0 @@
-class Estimate < ActiveRecord::Base
-  belongs_to :shipment
-  validates_presence_of :price, :carrier, :service_name
-  
-end
--- app/models/package.rb
+++
@@ -1,4 +0,0 @@
-class Package < ActiveRecord::Base
-  validates_presence_of :weight, :dimensions
-  belongs_to :shipment
-end
