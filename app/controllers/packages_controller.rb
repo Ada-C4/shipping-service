6997acdf7bf75f9cae7a574dd789e32ba5e91059
noglows <jessica.noglows@outlook.com>
2016-01-22 09:38:07 -0800

    Added USPS back in

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -7,25 +7,32 @@ class PackagesController < ApplicationController
       origin = create_origin(params)
       destination = create_destination(params)
 
+      begin
+        # Tries to get both UPS and USPS. Sometimes USPS fails so the sub-rescue method will return just UPS if hit
+        usps = ActiveShipping::USPS.new(login: ENV["USPS_USERNAME"], password: ENV["USPS_PASSWORD"])
+        usps_response = usps.find_rates(origin, destination, package)
+        usps_rates = usps_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
-      # Verified USPS works
-      # usps = ActiveShipping::USPS.new(login: ENV["USPS_USERNAME"], password: ENV["USPS_PASSWORD"])
-      # response = usps.find_rates(origin, destination, package)
-      # usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+        ups = ActiveShipping::UPS.new(login: ENV["UPS_ACCOUNT_NAME"], password: ENV["UPS_ACCOUNT_PASSWORD"], key: ENV["UPS_ACCESS_KEY"])
+        ups_response = ups.find_rates(origin, destination, package)
+        ups_rates = ups_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+        #
+        # fedex = ActiveShipping::FedEx.new(:test => true, login: ENV["FEDEX_LOGIN"], password: ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACCOUNT"])
+        # response = fedex.find_rates(origin, destination, package)
+        # fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
-      # Verified UPS works
-      ups = ActiveShipping::UPS.new(login: ENV["UPS_ACCOUNT_NAME"], password: ENV["UPS_ACCOUNT_PASSWORD"], key: ENV["UPS_ACCESS_KEY"])
-      response = ups.find_rates(origin, destination, package)
-      ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-      #
-      # fedex = ActiveShipping::FedEx.new(:test => true, login: ENV["FEDEX_LOGIN"], password: ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACCOUNT"])
-      # response = fedex.find_rates(origin, destination, package)
-      # fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+        all_rates = { ups: ups_rates, usps: usps_rates}
+        render :json => all_rates.as_json
 
-      all_rates = { ups: ups_rates}
-#      , fedex: fedex_rates 
+      rescue
+        ups = ActiveShipping::UPS.new(login: ENV["UPS_ACCOUNT_NAME"], password: ENV["UPS_ACCOUNT_PASSWORD"], key: ENV["UPS_ACCESS_KEY"])
+        ups_response = ups.find_rates(origin, destination, package)
+        ups_rates = ups_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
-      render :json => all_rates.as_json
+        all_rates = { ups: ups_rates }
+        render :json => all_rates.as_json
+      end
+
     rescue
       render :json => [], :status => :no_content
     end
