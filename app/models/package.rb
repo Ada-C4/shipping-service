07aa844446d45954543fa851a2e253fc40491984
Tammy <misstamar@gmail.com>
2016-01-22 11:03:28 -0800

    commit before pulling

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,30 +1,4 @@
 class Package < ActiveRecord::Base
 
-  def get_package
-    package = [
-      ActiveShipping::Package.new(100,
-                                  [100,100],
-                                  value: 10000)
-              ]
 
-    return package
-  end
-
-  def get_origin
-    origin = ActiveShipping::UPS.new(country: 'US',
-                                      state: 'WA',
-                                      city: 'Seattle',
-                                      zip: 98122)
-    return origin
-  end
-
-  def get_destination(hash)
-    destination = ActiveShipping::UPS.new(
-          country: 'US',
-          state: hash[:state],
-          city: hash[:city],
-          zip: hash[:zip])
-
-    return destination
-  end
 end
