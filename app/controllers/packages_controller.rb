c19181bfcfefed71717aa70b4aaa6ab1916624d9
Tammy <misstamar@gmail.com>
2016-01-21 16:08:13 -0800

    added env file

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -3,24 +3,25 @@ require 'httparty'
 class PackagesController < ApplicationController
 
   def receive_info
-    @hash = 
+    hash = params
+    raise
   end
-  def ups_price_estimate
-
-
-  end
-
-  def usps_price_estimate
-
-  end
-
-  def ups_delivery_time
-
-  end
-
-  def usps_delivery_time
-
-  end
+  #
+  # def ups_price_estimate
+  #
+  # end
+  #
+  # def usps_price_estimate
+  #
+  # end
+  #
+  # def ups_delivery_time
+  #
+  # end
+  #
+  # def usps_delivery_time
+  #
+  # end
 
 
 
