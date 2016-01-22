94fb043fcae415afd44815a44731b3abbf4c3a4a
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-22 09:54:31 -0800

    moved model methods to the controller. all (3) failing tests

--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Package, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:package){ ActiveShipping::Package.new(
+    100,
+    [100,100],
+    value: 10000
+    )
+  }
+
+  describe "#get_package" do
+    it "creates a package object" do
+      expect(package).to be_an_instance_of Package
+    end
+  end
+
+
 end
