6b8ef7f3bc2e2594b14d9e14a4ef9f40c4389dd6
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-21 16:50:32 -0800

    complete json response for a UPS estimate request

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -16,20 +16,44 @@ class EstimatesController < ApplicationController
   DIMENSIONS = [15, 10, 4.5]
 
   def estimate
-    # assume carrier is ups for now
-    # only one package for all products, for now
+    if params[:carrier] == "UPS"
+    # only one package for all products in order, for now
       package = [ActiveShipping::Package.new(OUNCES, DIMENSIONS, units: :imperial, value: params[:value])]
     # destination address info comes from query params provided from betsy app's API call
       destination = ActiveShipping::Location.new(country: COUNTRY, state: params[:destination][:state], city: params[:destination][:city], zip: params[:destination][:zip])
 
-
       rates_response = UPS.find_rates(ORIGIN, destination, package)
       delivery_dates_response = UPS.get_delivery_date_estimates(ORIGIN, destination, package)
 
-      # need to filter above responses down to the data we want (cost, date, service code)
-      # also need to remove any results with nil for service code
-      response = [rates_response.rates.first, delivery_dates_response.delivery_estimates.first]
+      # shipping_estimate will be a hash made up of service_code keys
+      # each key points to a hash which contains the cost and delivery date estimate
+      # for the corresponding service type.
+      shipping_estimate = Hash.new
+      rates_response.rates.each do |rate|
+        # skip this rate if service code is nil
+        next if rate.service_code.nil?
+        # otherwise, create a key for the corresponding service code
+        # the value is a hash containing the corresponding cost
+        shipping_estimate["#{rate.service_code}"] = { "cost" => "#{rate.total_price}" }
+      end
 
+      delivery_dates_response.delivery_estimates.each do |estimate|
+        # skip this estimate if service code is nil
+        next if estimate.service_code.nil?
+        # otherwise check to see if the service code already exists in the hash
+        if !shipping_estimate["#{estimate.service_code}"].nil?
+          # if it already exists, merge the date corresponding to this estimate into the the
+          # hash associated with this service code key
+          shipping_estimate["#{estimate.service_code}"].merge!({ "date" => "#{estimate.date}"})
+        else
+          # if the service code doesn't yet exist in the hash, create a key for it
+          # with the value being a hash corresponding to the date
+          shipping_estimate["#{estimate.service_code}"] = { "date" => "#{estimate.date}" }
+        end
+      end
+      # response is the resulting shipping_estimate hash created above
+      response = shipping_estimate
+    end
     render :json => response.as_json, :status => :ok
   end
 end
