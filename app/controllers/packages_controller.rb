02b6c49b991a653d069f34be06cbfd8afc1f8d63
noglows <jessica.noglows@outlook.com>
2016-01-22 13:49:05 -0800

    Cleaned up error handling

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,60 +1,54 @@
 class PackagesController < ApplicationController
-  skip_before_action :verify_authenticity_token
 
   def rates
     begin
-      package = create_package(params)
-      origin = create_origin(params)
-      destination = create_destination(params)
+      #package = create_package(params)
+      package = ActiveShipping::Package.new(params[:package][:weight].to_i, [params[:package][:length].to_i, params[:package][:width].to_i, params[:package][:height].to_i], :units => params[:package][:units])
 
+    rescue
+      render :json => ["Incorrect or missing parameters for package"], :status => :bad_request
+    end
+
+    begin
+      origin = ActiveShipping::Location.new(params[:origin])
+    rescue
+      render :json => ["Incorrect or missing parameters for origin address"], :status => :bad_request
+    end
+
+    begin
+      destination = ActiveShipping::Location.new(params[:destination])
+    rescue
+      render :json => ["Incorrect or missing parameters for destination address"], :status => :bad_request
+    end
+
+    begin
+      # Tries to get both UPS and USPS. Sometimes USPS fails so the sub-rescue method will return just UPS if hit
+      usps = ActiveShipping::USPS.new(login: ENV["USPS_USERNAME"], password: ENV["USPS_PASSWORD"])
+      usps_response = usps.find_rates(origin, destination, package)
+      usps_rates = usps_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+
+      ups = ActiveShipping::UPS.new(login: ENV["UPS_ACCOUNT_NAME"], password: ENV["UPS_ACCOUNT_PASSWORD"], key: ENV["UPS_ACCESS_KEY"])
+      ups_response = ups.find_rates(origin, destination, package)
+      ups_rates = ups_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+      #
+      # fedex = ActiveShipping::FedEx.new(:test => true, login: ENV["FEDEX_LOGIN"], password: ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACCOUNT"])
+      # response = fedex.find_rates(origin, destination, package)
+      # fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+
+      all_rates = { ups: ups_rates, usps: usps_rates}
+      render :json => all_rates.as_json
+
+    rescue
       begin
-        # Tries to get both UPS and USPS. Sometimes USPS fails so the sub-rescue method will return just UPS if hit
-        usps = ActiveShipping::USPS.new(login: ENV["USPS_USERNAME"], password: ENV["USPS_PASSWORD"])
-        usps_response = usps.find_rates(origin, destination, package)
-        usps_rates = usps_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-
-        ups = ActiveShipping::UPS.new(login: ENV["UPS_ACCOUNT_NAME"], password: ENV["UPS_ACCOUNT_PASSWORD"], key: ENV["UPS_ACCESS_KEY"])
-        ups_response = ups.find_rates(origin, destination, package)
-        ups_rates = ups_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-        #
-        # fedex = ActiveShipping::FedEx.new(:test => true, login: ENV["FEDEX_LOGIN"], password: ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACCOUNT"])
-        # response = fedex.find_rates(origin, destination, package)
-        # fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-
-        all_rates = { ups: ups_rates, usps: usps_rates}
-        render :json => all_rates.as_json
-
-      rescue
         ups = ActiveShipping::UPS.new(login: ENV["UPS_ACCOUNT_NAME"], password: ENV["UPS_ACCOUNT_PASSWORD"], key: ENV["UPS_ACCESS_KEY"])
         ups_response = ups.find_rates(origin, destination, package)
         ups_rates = ups_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
         all_rates = { ups: ups_rates }
         render :json => all_rates.as_json
+      rescue
+        render :json => "There has been an error", :status => :no_content
       end
-
-    rescue
-      render :json => [], :status => :no_content
     end
-  end
-
-  private
-
-  def create_package(params)
-    @package = ActiveShipping::Package.new(params[:package][:weight].to_i, [params[:package][:length].to_i, params[:package][:width].to_i, params[:package][:height].to_i], :units => params[:package][:units])
-  end
-
-  def create_destination(params)
-    @destination = ActiveShipping::Location.new(country: params[:destination][:country],
-                                            state: params[:destination][:state],
-                                            city: params[:destination][:city],
-                                            zip: params[:destination][:zip])
-  end
-
-  def create_origin(params)
-    @origin = ActiveShipping::Location.new(country: params[:origin][:country],
-                                            state: params[:origin][:state],
-                                            city: params[:origin][:city],
-                                            zip: params[:origin][:zip])
   end
 end
